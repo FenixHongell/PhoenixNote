f704b31ba3c95a25688e7cefd2df328aa606ba7a
Fenix Hongell <105488638+FenixHongell@users.noreply.github.com>
2023-08-14 10:53:39 +0300

    math now working

--- src/components/QuillWindow.jsx
+++ src/components/QuillWindow.jsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import 'react-quill/dist/quill.snow.css';
+import 'katex/dist/katex.min.css';
 export default class QuillWindow extends React.Component {
    constructor(props) {
       super(props)
       if (document) {
          this.quill = require('react-quill')
          this.focused = false;
+         window.katex = require("katex");
       }
    }
 
@@ -16,6 +18,12 @@ export default class QuillWindow extends React.Component {
       if (Quill) {
          return (
              <Quill
+                 modules={{
+                    toolbar: [
+                        [{ header: [1, 2, 3, false] }], ["bold", "italic", "underline", "strike"],
+                        ["formula"], [{ color: [] }, { background: [] }],  [{ align: [] }], ["clean"]
+                    ]
+                 }}
                  onFocus={() => this.focused = true}
                  onBlur={() => this.focused = false}
                  className={"min-h-[100px]"}
